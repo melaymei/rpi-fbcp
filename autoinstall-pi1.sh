dd1cf8e8e0933998db620b880a5182b1d4c54a23
wdmomoxx <540825306@qq.com>
2018-08-16 10:32:40 +0800

    Update autoinstall-pi1.sh

--- autoinstall-pi1.sh
+++ autoinstall-pi1.sh
@@ -29,6 +29,7 @@ sudo sed -i '$a spi-bcm2835' modules
 sudo sed -i '$a fbtft_device' modules
 cd /boot/
 sudo sed -i 's/#dtparam=spi=on/dtparam=spi=on/g' config.txt
+sudo sed -i '$a dtoverlay=pwm-2chan,pin=18,func=2' config.txt
 sudo sed -i '$a dtoverlay=pitft28-resistive,rotate=270,speed=62000000,fps=60' config.txt
 sudo sed -i '$a hdmi_force_hotplug=1' config.txt
 sudo sed -i '$a hdmi_group=2' config.txt

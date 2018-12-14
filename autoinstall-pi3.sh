a637a69055fdcc8a097c9748dcb983f0cb0bf6aa
melaymei <41831233+melaymei@users.noreply.github.com>
2018-12-14 10:45:07 +0800

    Update autoinstall-pi3.sh

--- autoinstall-pi3.sh
+++ autoinstall-pi3.sh
@@ -29,14 +29,10 @@ sudo sed -i '$a spi-bcm2835' modules
 sudo sed -i '$a fbtft_device' modules
 cd /boot/
 sudo sed -i 's/#dtparam=spi=on/dtparam=spi=on/g' config.txt
-sudo sed -i '$a dtoverlay=pwm-2chan,pin=18,func=2' config.txt
 sudo sed -i '$a dtoverlay=pitft28-resistive,rotate=270,speed=90000000,fps=60' config.txt
 sudo sed -i '$a hdmi_force_hotplug=1' config.txt
 sudo sed -i '$a hdmi_group=2' config.txt
 sudo sed -i '$a hdmi_mode=87' config.txt
 sudo sed -i '$a hdmi_cvt=320 240 60 1 0 0 0' config.txt
-sudo rm -rf rpi-fbcp/
-sudo rm -rf rpi-fbcp-master/
 cd ~
-sudo rm -rf rpi-fbcp/
 sudo reboot
